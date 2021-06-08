acbea12ccdf783af285e3c41909477e1e6fedef5
Xiaochen Xu <xiaochex@gmail.com>
2021-06-07 22:57:54 -0400

    commit tree 4444eaeaca83befde9bd527abcbee399caa929ab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
+  
 class Person
-  #your code here
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.class.attr_accessor(k)
+        self.send("#{k}=", v)
+      end
+    end
+  end
 end
